bdad4a91e5f2ab9784922f89f5b6def7dc4032d9
ChristianPickettCode <pickett.christian@outlook.com>
2021-01-07 19:08:40 -0500

    Added connect button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Dashboard from "./Dashboard/Dashboard";
-import Landing from "./Landing/Landing";
 
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
+import { Button } from "antd";
 
 function App (props) {
   const [user, setUser] = useState(props.user);
 
   useEffect(() => {
     setUser(props.user);
-  }, [props.user])
+    
+  }, [props.user]);
+
+  const connect = () => {
+    if(!props.user) { 
+      props.login(); 
+    }
+  }
   
   return (
     <div className="App">
       <Router>
-        { user ? 
-          <Dashboard user={user} {...props} />
-        : 
-          <Landing user={user} {...props} />
-        }
+        { user ?  <Dashboard user={user} {...props} /> : <div onClick={connect} style={{marginTop:"25%"}}><Button type="primary">Connect</Button></div>}
       </Router>
     </div>
   );
