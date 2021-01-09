f4dd6092c7acae1bc9f958406617dabd414bc8da
ChristianPickettCode <pickett.christian@outlook.com>
2021-01-09 15:47:49 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 import Dashboard from "./Dashboard/Dashboard";
 
 import {
-  BrowserRouter as Router,
+  BrowserRouter as Router
 } from "react-router-dom";
-import { Button, Spin } from "antd";
+import { Button, PageHeader, Spin } from "antd";
 
 function App (props) {
   const [user, setUser] = useState(props.user);
@@ -34,8 +34,11 @@ function App (props) {
         { loggedIn && user ?  
           <Dashboard user={user} {...props} /> 
           : loggedOut && !user ? 
-          <div onClick={connect} style={{marginTop:"18%"}}>
-            <h1>Atlis Dashboard</h1>
+          <div onClick={connect}>
+            <PageHeader
+                title={<a href="https://atlis.dev"><h3 style={{ margin:"0" }}>"Atlis."</h3></a>}
+                />
+            <h1 style={{marginTop:"4%"}}>Atlis Dashboard</h1>
             <Button type="primary">Connect</Button>
           </div> : <div style={{marginTop:"25%"}}><Spin /></div>}
       </Router>
